53af2513a628ab54d2fccaa788daccb2faf7a75b
Angel Reyes <miickeyreyez@gmail.com>
2018-07-17 21:23:59 -0500

    Sección 20: Se crea la función de autoplay

--- 03TicTacToyGame/app/src/main/java/com/udemy/a03tictactoygame/MainActivity.kt
+++ 03TicTacToyGame/app/src/main/java/com/udemy/a03tictactoygame/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.udemy.a03tictactoygame
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var p1 = ArrayList<Int>()
+    var p2 = ArrayList<Int>()
+    var active = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +34,77 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> id = 8
             R.id.button9 -> id = 9
         }
+        playGame(id, selected)
+    }
+
+    fun playGame(id: Int, selected: Button) {
+        if(active == 1) {
+            selected.text = "X"
+            selected.setBackgroundColor(Color.GREEN)
+            p1.add(id)
+            active = 2
+            autoPlay()
+        } else {
+            selected.text = "0"
+            selected.setBackgroundColor(Color.BLUE)
+            p2.add(id)
+            active = 1
+        }
+        selected.isEnabled = false
+        checkWinner()
+    }
+
+    fun checkWinner() {
+        var winner = -1
+        if(p1.contains(1) && p1.contains(2) && p1.contains(3)) {
+            winner = 1
+        }
+        if(p1.contains(1) && p1.contains(5) && p1.contains(9)) {
+            winner = 1
+        }
+        if(p2.contains(4) && p2.contains(5) && p2.contains(6)) {
+            winner = 2
+        }
+        if(p2.contains(3) && p2.contains(5) && p2.contains(7)) {
+            winner = 2
+        }
+
+        if(winner != -1) {
+            if(winner == 1) {
+                Toast.makeText(this, "P1 Wins!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "P2 Wins!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun autoPlay() {
+        var emptyCells = ArrayList<Int>();
+        for(empty in 1..9) {
+            if(!(p1.contains(empty)) || !(p2.contains(empty))) {
+                emptyCells.add(empty);
+            }
+        }
+
+        var r = Random();
+        val randIndex = r.nextInt(emptyCells.size - 1) + 1
+        val celId = emptyCells[randIndex]
+
+        var selected: Button
+        when(celId) {
+            1 -> selected = button
+            2 -> selected = button2
+            3 -> selected = button3
+            4 -> selected = button4
+            5 -> selected = button5
+            6 -> selected = button6
+            7 -> selected = button7
+            8 -> selected = button8
+            9 -> selected = button8
+            else -> {
+                selected = button
+            }
+        }
+        playGame(celId, selected)
     }
 }
